ea438b0ab88f86844fecbc7fcba0ed25aa215f98
cnridley <cnridley1@gmail.com>
2021-04-25 14:32:28 +0000

    add guide to bag view made and rendered on bag.html

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,17 +1,29 @@
-from decimal import Decimal 
+from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from guide.models import nutitionGuides
 
 # context processor to make the above dictionary available across the whole application
 def bag_contents(request):
 
     bag_items = []
     total = 0
-    product_count = 0 
+    product_count = 0
+    bag = request.session.get('bag', {})
+
+    for item_id, quantity in bag.items():
+        product = get_object_or_404(nutitionGuides, pk=item_id)
+        total = quantity * product.price
+        product_count += quantity
+        bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'product': product,
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE/100)
         free_delivery = settings.FREE_DELIVERY_THRESHOLD - total
-    
     else:
         delivery = 0
         free_delivery = 0
--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 # Create your views here.
 
 def bag(request):
@@ -6,3 +6,19 @@ def bag(request):
     """A view to return bag page"""
 
     return render(request, 'bag.html')
+
+
+def add_guides_to_bag(request, item_id):
+    """Add a guide to the shopping bag"""
+    quantity = 1
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
+
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+    else:
+        bag[item_id] = quantity
+
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    return redirect(redirect_url) 
